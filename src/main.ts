46d5c312427bdf633c970acc2d613c1a185ec2f0
Lucas Kennde <lucaskennde@gmail.com>
2024-09-23 12:23:07 -0300

    Atividade 2

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { Produto } from './models/produto.ts';
+import { Carrinho } from './models/Carrinho.ts';
+import { Vitrine } from './models/Vitrine.ts';
+import { Checkout } from './models/Checkout.ts';
+
+
+const produto1 = new Produto('Camisa', 'Roupas', 1, 50, 'foto1.jpg');
+const produto2 = new Produto('Tênis', 'Calçados', 2, 200, 'foto2.jpg');
+
+// Testando Carrinho
+const carrinho = new Carrinho();
+carrinho.adicionar(produto1, 2);
+carrinho.adicionar(produto2, 1);
+carrinho.listar();
+
+// Checkout
+const checkout = new Checkout('Rua ABC, 123', 'Cartão de Crédito', carrinho);
+checkout.processarPagamento();
+checkout.atualizaEstoque();
+
+// Testando a Vitrine
+const vitrine = new Vitrine();
+vitrine.adicionarDestaque(produto1);
+vitrine.adicionarCategoria('Roupas', produto1);
+vitrine.adicionarCategoria('Calçados', produto2);
+
+// Listando os produtos da vitrine
+vitrine.listarDestaque();
+vitrine.listarCategoria('Roupas');
+vitrine.listarCategoria('Calçados');
